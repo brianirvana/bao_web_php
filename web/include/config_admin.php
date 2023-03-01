7e3e48b07f0d56a3614d86528f4699c62b6945cd
Brian Sabatier <brianirvana@gmail.com>
2023-03-01 20:22:04 -0300

    Update config_admin.php

--- web/include/config_admin.php
+++ web/include/config_admin.php
@@ -2,7 +2,7 @@
 $usuarios_sesion="Staff";
 $sql_host="localhost";
 $sql_usuario="benderao_user";
-$sql_pass="3421321.Staff";
+$sql_pass="x#KOm,2!*+Si";
 $sql_db="benderao_web";
 $sql_tabla="usuarios";
 ?>
